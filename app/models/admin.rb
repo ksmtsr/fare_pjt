53e75eab13e8efc387c39b9cdc52dd255add2b78
s-kishimoto <sarii.4u@icloud.com>
2024-07-28 21:55:41 +0900

    管理者ログイン機能作成

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,21 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:username]
+
+         validates :password, presence: false, on: :create
+         validates :password_confirmation, presence: false, on: :create
+       
+         # デフォルトのDeviseのパスワード要件を無効にする
+         def password_required?
+           false
+         end
+       
+         # パスワードの確認要件を無効にする
+         def password_match?
+           true
+         end
+
+  validates :username, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # devise_for :employees
-  # devise_for :admins
+  devise_for :employees
+  devise_for :admins, controllers: {
+    registrations: 'admins/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
